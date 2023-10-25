6c7755bc0d99e9e9125f90f3a659a338e135ebae
Todd Medema <toddmedema@gmail.com>
2023-10-24 21:13:03 -0400

    Have scheduler write to disk

--- README.md
+++ README.md
@@ -4,10 +4,13 @@ pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-
 TODO
 
 - [ ] test download and schedule flow with real calendar
-- [ ] uploader
+- [ ] uploader.py
 
 v2
 
 - [ ] better scheduling
-	- take into account "to_schedule" count
+	- are all classes the same length? If not, add to metadata and apply bin-packing
+	- take into account "to_schedule" count so that it's not all one class
 	- sanity check class times (e.g. don't start before 7am or after 10pm?)
+	- if there are too many classes, how to prioritize? 
+	- if there are too few classes / spare times, how to decide which times are best?
--- scheduler.py
+++ scheduler.py
@@ -75,15 +75,23 @@ for c in classesData:
 for k in potential:
     random.shuffle(potential[k])
 
-# Construct candidate schedules, using i to try all permutations of <class, instructor, date>
-for i in range(5):
-    candidate = []
-    ci = i
-    for c in list(classesData):
-        if len(potential[c]) == 0:
-            continue
-        candidate.append((c, *(potential[c][ci % len(potential[c])])))
-        ci // len(potential[c])
+# Generate the schedule
+schedule = []
+for c in list(classesData):
+    if len(potential[c]) == 0:
+        continue
+    schedule.append((c, *(potential[c][1 % len(potential[c])])))
+    1 // len(potential[c])
+schedule.sort(key=lambda v: v[2])
 
-    candidate.sort(key=lambda v: v[2])
-    print([(c[0], c[1].name, c[2].isoformat()) for c in candidate])
+# Write to disk
+output = []
+for c in schedule:
+    output.append({
+        'name': c[0], 
+        'description': classesData[c[0]]['description'],
+        'instructor': c[1].name, 
+        'time': c[2].isoformat()
+    })
+with open("schedule.json", "w") as f:
+    json.dump({'schedule': output}, f, indent=4)
